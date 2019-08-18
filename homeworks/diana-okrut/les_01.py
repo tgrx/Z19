a96498bbd829ff869bcfbab967359568f3e89f93
diana-okrut <47084900+diana-okrut@users.noreply.github.com>
2019-08-18 13:33:53 +0300

    исправлен пробел после импорта, добавлена защита от введения в имени "ххх", добавлен для выбора вариант "выход" и возврат в меню при неправильно введенном символе при выборе. (#17)
    выполнен уровень задания 4

--- homeworks/diana-okrut/les_01.py
+++ homeworks/diana-okrut/les_01.py
@@ -1,16 +1,31 @@
 import os
-d = input("Здравствуйте, назовите свое имя: ")
-print(d, ", приятно познакомиться!")
-e = int(input("Какая информация вам интересна: первая программа новоиспеченного программиста (1), знакомство со мной (2)"
-              " или информация о вашей системе(3)?"))
-#хотела сделать вот так,.e = int(input(d, "Какая информация вам интересна: первая программа новоиспеченного программиста
-# (1), знакомство со мной (2) или информация о моем железе(3)?"))
-if e == 1:
-    print("Hello World!")
-elif e == 2:
-    print("My name is Diana. I am 25 years old. I am a student of python courses in school Teach Me Skills. "
-          "My english is very bad.")
-elif e == 3:
-    print(os.name())
-else:
-    print("Некорректные данные")
+
+tries = 0
+while tries != 100:
+    name = (input("Здравствуйте, назовите свое имя: "))
+    if name == "xxx":
+        print("Упс, вышла ошибка при вводе данных. Повторите ваш ответ:")
+    else:
+        break
+    tries += 1
+while tries != 100:
+    choice = int(input(f"{name} , какая информация вам интересна:"
+                       f" первая программа новоиспеченного программиста (1),"
+                       f" знакомство со мной (2)"
+                       f"или информация о вашей системе(3)?"
+                       f" Для выхода из программы нажмите (4). "))
+    if choice != 1 and choice != 2 and choice != 3 and choice != 4:
+        print("Упс, произошла ошибка при вводе данных. Повторите ваш ответ:")
+        tries += 1
+    else:
+        if choice == 1:
+            print("Hello World!")
+        elif choice == 2:
+            print("My name is Diana. I am 25 years old. "
+                  "I'm a student of python courses in school Teach Me Skills. "
+                  "My english is very bad.")
+        elif choice == 3:
+            print(os.name)
+        else:
+            print("Всего доброго!")
+        break
