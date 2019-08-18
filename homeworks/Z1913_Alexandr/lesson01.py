c537583a0658307fbd1947b7249b669059e31af7
AlexandrZ1913 <773821@gmail.com>
2019-08-19 01:25:11 +0300

    варианты ответа

--- homeworks/Z1913_Alexandr/lesson01.py
+++ homeworks/Z1913_Alexandr/lesson01.py
@@ -5,5 +5,9 @@ print("I work on a system " + os.name)
 name = input("What's your name? \n")
 print("Hello " + name + "!\nWhat do you want to do?")
 answer = input("Would you like to program? \n")
-if answer="yes"
-print("OK")
+if answer == "yes":  # выбор варианта ответа
+    print("Good! " + name)
+if answer == "no":
+    print("I'm sorry")
+else:
+    print("I did not understand the answer")
