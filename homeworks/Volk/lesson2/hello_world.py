10f5caea46d6aad86a361565fa2009d42ba210a2
Dalalaler <dalalaler@gmail.com>
2019-08-18 20:58:25 +0300

    Add another verification

--- homeworks/Volk/lesson2/hello_world.py
+++ homeworks/Volk/lesson2/hello_world.py
@@ -29,7 +29,20 @@ if a == 4:
     b= "Hello, " + b + "!"#Соединение строк
     print(b)
 if a == 5:
-    print("Change a variant (1-6)")
-    a = int(input())
+
+    while True:  # Выполняем бесконечно, пока не встретим break
+        try:
+            a = int(input("Enter your variant (1-6): "))
+        except ValueError:  # Выполняем проверку на тип int,
+            # Делаем через искючения exceptions, ValueError - функция получает аргумент правильного типа, но некорректного значения.
+            print("Variant is not an integer!")
+            continue
+        else:
+            if a in arr:  # Вместо банального сравнения решил поиграться со списками
+                print("Variant is available!")
+                break
+            else:
+                print("Variant is not 1-6!")
+
 if a == 6:
     webbrowser.open('https://youtu.be/dQw4w9WgXcQ')
