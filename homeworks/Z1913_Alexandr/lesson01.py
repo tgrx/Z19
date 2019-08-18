79fb7c6e72451b5525875f3d81240d4b8a4b38a0
AlexandrZ1913 <773821@gmail.com>
2019-08-19 01:07:14 +0300

    IF

--- homeworks/Z1913_Alexandr/lesson01.py
+++ homeworks/Z1913_Alexandr/lesson01.py
@@ -3,4 +3,7 @@ import os
 
 print("I work on a system " + os.name)
 name = input("What's your name? \n")
-print("Hello " + name+"!")
+print("Hello " + name + "!\nWhat do you want to do?")
+answer = input("Would you like to program? \n")
+if answer="yes"
+print("OK")
