14e9245d0b0d9211ee640de69a1a3dc9d0e25038
Evgen1177 <g5577@tut.by>
2019-08-18 21:49:49 +0300

    Версия до 4 пункта задания

--- homeworks/Evgen/lesson01.py
+++ homeworks/Evgen/lesson01.py
@@ -1,4 +1,7 @@
-print( "Hello world!")
-print ("I am Evgen!")
+print( "Hello world!\nI am Evgen!")
+#print ("I am Evgen!")
 import os
 print(os.name)
+input("What you name?: ")
+name="12345"
+print(name)
