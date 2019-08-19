924c052e78c9d6d2f384b495bcaa6a75fa6a951a
AlexandrZ1913 <773821@gmail.com>
2019-08-19 09:54:16 +0300

    варианты ответа с проверкой регистра

--- homeworks/Z1913_Alexandr/lesson01.py
+++ homeworks/Z1913_Alexandr/lesson01.py
@@ -5,9 +5,9 @@ print("I work on a system " + os.name)
 name = input("What's your name? \n")
 print("Hello " + name + "!\nWhat do you want to do?")
 answer = input("Would you like to program? \n")
-if answer == "yes":  # выбор варианта ответа
+if answer == "yes" or answer == "Yes" or answer == "YES":  # выбор варианта ответа с проверкой регистра
     print("Good! " + name)
-if answer == "no":
+elif answer == "no" or answer == "No" or answer == "NO":
     print("I'm sorry")
 else:
-    print("I did not understand the answer")
+    print("I did not understand the answer")  # если ответ не да ни нет
