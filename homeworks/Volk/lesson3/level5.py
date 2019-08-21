9e317d88e87adbefeebc976b0ec480d25d3ebbd3
Dalalaler <dalalaler@gmail.com>
2019-08-21 03:59:21 +0300

    First version of lvl5

---
+++ homeworks/Volk/lesson3/level5.py
@@ -0,0 +1,13 @@
+queue = []
+queue.append('Monday')
+queue.append('Tuesday')
+queue.append('Wednesday')
+queue.append('Thursday')
+queue.append('Friday')
+queue.append('Saturday')
+queue.append('Sunday')
+
+
+print(queue)
+
+print(q.pop(0))  # 'eat'
