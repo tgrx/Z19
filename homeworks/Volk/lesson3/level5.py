a2f0e4a9248a1a627ca8863138f657e15fd4fc29
Dalalaler <dalalaler@gmail.com>
2019-08-24 19:05:44 +0300

    Changed lvl5

--- homeworks/Volk/lesson3/level5.py
+++ homeworks/Volk/lesson3/level5.py
@@ -1,37 +1,17 @@
-from collections import deque
-
-class Queue():
-
-    def __init__(self, max_size = 10):
-
-        self._queue = deque(maxlen=max_size)
-
-    def enqueue(self, item):
-        self._queue.appendleft(item)
-
-    def dequeue(self):
-        return self._queue.pop()
-
-queue = Queue()
-queue.enqueue('Monday')
-queue.enqueue('Tuesday')
-queue.enqueue('Wednesday')
-queue.enqueue('Thursday')
-queue.enqueue('Friday')
-queue.enqueue('Saturday')
-queue.enqueue('Sunday')
-
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
+def enqueue(l, e):
+    l.append(e)
+    print(l)
 
 
-#Code under this comment doesnt working, but i have code with a simple working, it will be added in file level 5.1
-'''if __name__ == "__main__":       
+def dequeue(l):
+    if l:
+        return l.pop(0)
+    elif l == []:
+        print("Queue is empty!")
+        return None
+
+
+if __name__ == "__main__":
     x = []
     assert dequeue(x) is None
     assert enqueue(x, 1) is None
@@ -41,9 +21,4 @@ print(queue.dequeue())
     assert dequeue(x) == 2
     assert dequeue(x) == 3
     assert dequeue(x) is None
-    assert x == []'''
-
-
-
-
-
+    assert x == []
