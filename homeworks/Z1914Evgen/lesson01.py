dabf5fa752e7e1311ddb4e231a10d042804b5217
Evgen1177 <g5577@tut.by>
2019-08-24 18:26:57 +0300

    Сделано до 5 пункта Окончательный вариант

--- homeworks/Z1914Evgen/lesson01.py
+++ homeworks/Z1914Evgen/lesson01.py
@@ -1,12 +1,11 @@
 import os
 
-
-print ("Hello World\nI am Lenovo")
-print ("I Work on a systeme "+ os.name)
+print("Hello World\nI am Lenovo")
+print("I Work on a systeme " + os.name)
 name = input("What you name?\n")
-print ("Hello "+ name)
+print("Hello " + name)
 i = input("do you like programming\n")
 if i == "yes":
-    print ("good")
+    print("good")
 else:
     print("bad")
