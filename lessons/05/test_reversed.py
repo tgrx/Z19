5701188bf7622a335db742a4c4771e6d7865048a
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-08-27 02:59:17 +0300

    Travis CI (#44)
    * Added travis, hw check, blacked code
    
    * Added flake8
    
    * Added pylint
    
    * Suppress line length check and docstrings check by Pylint
    
    * Added check for github usernames
    
    * Added tests
    
    * Bypass other checks
    
    * added missing newline

--- lessons/05/test_reversed.py
+++ lessons/05/test_reversed.py
@@ -1,22 +1,24 @@
-import importlib.util
-import sys
 from pathlib import Path
 from typing import Callable
 
-fp = sys.argv[1]
-md = Path(sys.argv[1]).resolve()
-assert md.is_file(), f"{fp} is not a file"
-assert md.as_posix()[-3:] == ".py", f"{fp} is not a Python module"
 
-spec = importlib.util.spec_from_file_location("module.name", md.as_posix())
+def wants_path(pth: Path) -> bool:
+    return "lesson05" in pth.name
 
-md = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(md)
-assert hasattr(md, "reversed2"), f"no `reversed2` in {fp}"
 
-reversed2 = getattr(md, "reversed2")
-assert isinstance(reversed2, Callable), f"{reversed2} is not callable"
+def wants_module(module) -> bool:
+    return True
 
-assert reversed2([]) == []
-assert reversed2([3, 2, 1]) == [1, 2, 3]
-assert reversed2([1, 1, 1]) == [1, 1, 1]
+
+def verify(module):
+    name = "reversed2"
+
+    assert hasattr(module, name), f"no `{name}` defined in {module}"
+
+    reversed2 = getattr(module, "reversed2")
+
+    assert isinstance(reversed2, Callable), f"`{name}` is not callable"
+
+    assert reversed2([]) == []
+    assert reversed2([3, 2, 1]) == [1, 2, 3]
+    assert reversed2([1, 1, 1]) == [1, 1, 1]
