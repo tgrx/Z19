4cf514587f3f359af777f320208643b41bbad7df
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-08-27 02:13:44 +0300

    Added check for github usernames

--- check_homeworks.sh
+++ check_homeworks.sh
@@ -15,7 +15,49 @@ set -o pipefail
 
 # ---------------------------------------------------------
 
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+
+assert_names() {
+  dir_names=$(ls -d homeworks/* | sed -e 's/homeworks\///g')
+  github_names=(
+    "AlexandrZ1913"
+    "Dalalaler"
+    "daniluuuuuuuk"
+    "davidovichanton"
+    "diana-okrut"
+    "Evgen1177"
+    "Kefir331"
+    "lizaveta-stasevich"
+    "maximilian-sklyar"
+    "OswinOswalt"
+    "PierreBykov"
+    "python77777121"
+  )
+
+  for n in ${dir_names}; do
+    if [[ $(contains "${github_names[@]}" "$n") == "n" ]]; then
+      echo "unknown Github name in homeworks/: '$n'"
+      return 1
+    fi
+  done
+}
+
+# ---------------------------------------------------------
+
 # check code format
+
+assert_names || abort "HOMEWORKS USERS"
 
 python -m black --check . || abort 'CODE COLOR'
 
