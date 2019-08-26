32c18ff3609683099019909d3dce943905dbcd8c
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-08-27 01:29:00 +0300

    Added pylint

--- check_homeworks.sh
+++ check_homeworks.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 # ---------------------------------------------------------
-abort()
-{
-    if test -n "$1"; then
-        echo >&2 -e "\n\nFAILED: $1\n\n"
-    fi
-    exit 1
+abort() {
+  if test -n "$1"; then
+    echo >&2 -e "\n\nFAILED: $1\n\n"
+  fi
+  exit 1
 }
 
 trap 'abort' 0
@@ -21,6 +20,8 @@ set -o pipefail
 python -m black --check . || abort 'CODE COLOR'
 
 python -m flake8 homeworks/ || abort 'CODE STYLE'
+
+python -m pylint homeworks/ || abort 'CODE QUALITY'
 
 # ---------------------------------------------------------
 trap : 0
