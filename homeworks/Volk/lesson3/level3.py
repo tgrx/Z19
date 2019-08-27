8afcea03cda48240341152b8093d7a92a4a22ce6
Dalalaler <dalalaler@gmail.com>
2019-08-27 18:25:59 +0300

    Merge branch 'master' of /home/danil/PycharmProjects/volk/lesson_1/Z19 with conflicts.

--- homeworks/Volk/lesson3/level3.py
+++ homeworks/Volk/lesson3/level3.py
@@ -1,4 +1,15 @@
-while True:
+def f(x, y=[]):
+    y.append(x)
+    return y
+print(f(1,))
+print(f(1,))
+print(f(1,))
+print(f(1,))
+print(f(1,))
+
+
+
+'''while True:
   try:
      length = int(input("Enter length of list: "))
   except ValueError:
@@ -12,4 +23,4 @@ while True:
 x = list()
 while length > 0:  #my ubuntu has died when length raised to 1000000000, now I know why commit is need for
     x.append("a")
-    length -= 1
+    length -= 1'''
