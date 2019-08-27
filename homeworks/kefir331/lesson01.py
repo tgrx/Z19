1515f0b3d851df3a7e70b9b5d23033c752a724ec
kefir331 <Seven1486>
2019-08-27 12:40:55 +0300

    fix2

--- homeworks/kefir331/lesson01.py
+++ homeworks/kefir331/lesson01.py
@@ -1,2 +1,18 @@
-input()
-print("hello world")
+from math import pi
+
+
+
+print('Hello World!')
+from decimal import Decimal as D
+
+a = 10
+b = 5
+c = a * b
+e = a + c
+d = b / e
+print(D(c))
+print(D(d))
+print(c)
+print(d)
+print(e)
+print(c**d)
