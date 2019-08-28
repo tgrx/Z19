7094cd86f73b0df99210fe07d0aa06d9196eacee
kefir331 <Seven1486>
2019-08-28 17:08:17 +0300

    fix2

--- homeworks/kefir331/lesson01.py
+++ homeworks/kefir331/lesson01.py
@@ -5,6 +5,15 @@ from math import pi
 print('Hello World!')
 from decimal import Decimal as D
 
+
+name = input("Введите свое имя: ")
+age = input("Введите свой возраст: ")
+
+print("Привет" + name + "!")
+print("Тебе уже" + age + "!")
+
+
+
 a = 10
 b = 5
 c = a * b
@@ -16,3 +25,11 @@ print(c)
 print(d)
 print(e)
 print(c**d)
+
+print(input("как тебя зовут?"))
+
+a = input("введите первое число:")
+b = input("введите второе число:")
+
+print(int(a) * int(b))
+
--- homeworks/kefir331/lesson05.py
+++ homeworks/kefir331/lesson05.py
@@ -1,7 +1,30 @@
-name = input("Введите свое имя: ")
-age = input("Введите свой возраст: ")
-
-print("Привет" + name + "!")
-print("Тебе уже" + age + "!")
+## Уровень 2
 
 
+a = [1,2,3,4,5]
+
+print(list(reversed(a)))
+
+## Уровень 3
+
+b = [2, 4, 7, 1, 3, 5, 6]
+
+print(list(sorted(b)))
+
+## Уровень 4
+
+ def is_leap(year: int):
+  """Проверяет, високосный ли год"""
+  if year % 400 == 0:
+            return True
+    if year % 100 == 0:
+       return False
+    if year % 4 == 0:
+      return True
+   return False
+
+years = [2000,2004,2019]
+x = filter(is_leap, years)
+print(list(x))
+
+bool
