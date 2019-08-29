80bacb1f14babe3334d5656aeddf2b9b85ba9b15
Diana Okrut <zyndel94@gmail.com>
2019-08-29 17:08:07 +0300

    added TypedReversed and LazyReversed

--- homeworks/zyndel_05/lesson05.py
+++ homeworks/zyndel_05/lesson05.py
@@ -2,35 +2,80 @@ from datetime import date as d, timedelta as t
 
 
 def Reversed(l):
-    if type(l)is not (list or str or tuple):
-        print('Введенные данные не являются неизменяемой коллекцией')
+    '''функция возвращает список из элементов коллекции-аргумента, но которые в обратном порядке'''
+    types = (str, tuple, list)
+    if type(l) not in types:
+        print('Введенные данные не соответствуют условию.')
     else:
-        l = list(l)
         new_l = []
-        n = 0
+        count = 0
         for i in l:
-            x = l[n]
-            new_l.insert(0,x)
-            n += 1
+            x = l[count]
+            new_l.insert(0, x)
+            count += 1
         return new_l
 
 
-def Sorted(x):
-    x = list(x)
-    new_x = []
-    for i in x:
-        if x[0] < i:
-            new_x.append(i)
-    return new_x
-#
+ # def Sorted(x):
+ #     '''функция возвращает список из элементов коллекции-аргумента, которые отсортированы по возрастанию'''
+ #     new_x = []
+ #     a = None
+ #     for i in x:
+ #         a = i
+ #         if x[0] <= i:
+ #             new_x.append(i)
+ #     return new_x
+
+
 # def Filter(foo, coll):
 #     l = []
 #     for c in coll:
 #         if foo(c):
 #             l.append(c)
-#
-#
-#
+
+
+def TypedReversed(l):
+    '''функция возвращает коллекцию того же типа, который имеет аргумент, но в обратном порядке'''
+    types = (str, tuple, list)
+    if type(l) not in types:
+        print('Введенные данные не соответствуют условию.')
+    else:
+        new_l = []
+        count = 0
+        for i in l:
+            x = l[count]
+            new_l.insert(0, x)
+            count += 1
+        if type(l) is str:
+            return ''.join(new_l)
+        elif type(l) is tuple:
+            return tuple(new_l)
+        else:
+            return new_l
+
+
+def LazyReversed(l):
+    '''функция возвращает итератор коллекции того же типа, который имеет аргумент, но в обратном порядке'''
+    types = (str, tuple, list)
+    if type(l) not in types:
+        print('Введенные данные не соответствуют условию.')
+    else:
+        new_l = []
+        count = 0
+        for i in l:
+            x = l[count]
+            new_l.insert(0, x)
+            count += 1
+        if type(l) is str:
+            a = ''.join(new_l)
+            return iter(a)
+        elif type(l) is tuple:
+            a = tuple(new_l)
+            return iter(a)
+        else:
+            return iter(new_l)
+
+
 # def DateRange(tomorrow,next_week):
 #     today = d.today()
 #     tomorrow = today + t(days=1)
