3172b4d1ffae1f3911fd1f65068ac292c02ab430
AlexandrZ1913 <54038260+AlexandrZ1913@users.noreply.github.com>
2019-08-31 12:21:27 +0300

    lesson 1 (#9)
    * lesson 1
    
    * lesson 1
    
    * Начало задания
    
    * IF
    
    * варианты ответа
    
    * варианты ответа с проверкой регистра
    
    * Окончательны вариант с циклом
    
    * Окончательный вариант с циклом и форматированием
    
    * Варианты вывода цифр
    
    * Renamed `alexander_basovets` package
    
    * code cleanup

---
+++ homeworks/alexandr_basovets/lesson01.py
@@ -0,0 +1,18 @@
+import os
+
+print("Hello, World!!!\nI am HAL 9000")
+print("I work on a system " + os.name)
+name = input("What's your name? \n")
+print("Hello " + name + "!\nWhat do you want to do?")
+i = 0
+while i != 1:  # цикл пока не получим ответ да или нет
+    answer = input("Would you like to program? \n")
+    if answer == "yes" or answer == "Yes" or answer == "YES":  # выбор варианта ответа с проверкой регистра
+        print("Good! " + name)
+        i = 1
+    elif answer == "no" or answer == "No" or answer == "NO":
+        print("I'm sorry")
+        i = 1
+    else:
+        print("I did not understand the answer")  # если ответ не да ни нет
+        i = 0
