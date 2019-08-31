22cc3640a76af98c7e5e121a2ef66653269030a8
AlexandrZ1913 <54038260+AlexandrZ1913@users.noreply.github.com>
2019-08-31 12:43:46 +0300

    New z1913 24 (#37)
    * lesson 1
    
    * lesson 1
    
    * Начало задания
    
    * IF
    
    * варианты ответа
    
    * варианты ответа с проверкой регистра
    
    * Окончательны вариант с циклом
    
    * Окончательный вариант с циклом и форматированием
    
    * Варианты вывода цифр
    
    * Варианты вывода цифр
    Новая ветка
    
    * Варианты вывода цифр
    Новая ветка 2
    
    * Окончательный вариант 1 урока
    
    * Окончательный вариант 1 урока
    с комментариями
    
    * Третий урок уровень 2
    
    * Третий урок уровень 2 и 5
    
    * Третий урок уровень 2 и 5 .
    
    * code cleanup

--- homeworks/alexander_basovets/lesson01.py
+++ homeworks/alexander_basovets/lesson01.py
@@ -5,7 +5,7 @@ print("I work on a system " + os.name)
 name = input("What's your name? \n")
 print("Hello " + name + "!\nWhat do you want to do?")
 i = 0
-while i != 1:  # цикл пока не получим ответ да или нет
+while i != 1:  # цикл пока не получим ответ да или нет в зависимости от регистра
     answer = input("Would you like to program? \n")
     if answer == "yes" or answer == "Yes" or answer == "YES":  # выбор варианта ответа с проверкой регистра
         print("Good! " + name)
