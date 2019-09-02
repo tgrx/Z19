e3c44c9591452f42cca4ea3b8f775fb8775fe6e0
kefir331 <Seven1486>
2019-09-02 20:29:11 +0300

    fix1

--- homeworks/pavel_titov/lesson01.py
+++ homeworks/pavel_titov/lesson01.py
@@ -2,14 +2,9 @@ from decimal import Decimal as D
 
 print("Hello World!")
 
-a = 10
-b = 5
-c = a * b
-e = a + c
-d = b / e
-print(D(c))
-print(D(d))
-print(c)
-print(d)
-print(e)
-print(c ** d)
+name = input("Как тебя зовут?:")
+
+print ("Ваше имя:" + name + "!")
+
+import os
+print(os.name)
