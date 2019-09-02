043dc3f6355a0de0fa5b0ee887f9e5c449b6d155
Z1904 <david20toxa@gmail.com>
2019-09-02 20:53:58 +0300

    добавил проверку

--- homeworks/anton_davidovich/Davidovich.py
+++ homeworks/anton_davidovich/Davidovich.py
@@ -1,17 +1,26 @@
 import platform
 
-# print("hello world")
-# print("асталовиста бейби")
-# print("system:", platform.system())
-
 name = input("name: ")
 print(f"Hello, {name}")
-choise = int(input("выбирите вариант: 1, 2,3 "))
-if choise == 1:
+
+while True:
+    try:
+        choose = int(input("выбирите вариант: 1, 2,3 "))
+    except ValueError:
+        print("Только числа!")
+        continue
+    if choose in range(1, 4):
+        break
+    print("Только от 1 до 3!")
+    continue
+
+
+
+if choose == 1:
     print("hello world")
-elif choise == 2:
-    print("Меня зовут Антон - япродовец техники xiaomi")
-elif choise == 3:
-    print("system:", platform.system())
+elif choose == 2:
+    print("Меня зовут Антон - я продовец техники xiaomi")
+elif choose == 3:
+    print(f"system: {platform.system()} ")
 else:
     print("Вели не правильный вариант")
