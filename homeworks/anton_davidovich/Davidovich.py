2915c8b0931095f8f15c5ed306e57db1570de571
Z1904 <david20toxa@gmail.com>
2019-09-02 20:11:34 +0300

    задания урок 1

--- homeworks/anton_davidovich/Davidovich.py
+++ homeworks/anton_davidovich/Davidovich.py
@@ -1,6 +1,17 @@
 import platform
 
-print("hello world")
-print("асталовиста бейби")
+# print("hello world")
+# print("асталовиста бейби")
+# print("system:", platform.system())
 
-print("system:", platform.system())
+name = (input("name: "))
+print(f"Hello, {name}")
+choise = int(input("выбирите вариант: 1, 2,3 "))
+if choise == 1:
+    print("hello world")
+elif choise == 2:
+    print("Меня зовут Антон - япродовец техники xiaomi")
+elif choise == 3:
+    print("system:", platform.system())
+else:
+    print("Вели не правильный вариант")
