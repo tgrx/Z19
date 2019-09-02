400631970283a9213bae1cd8fb2d9e9767f38e0b
Dalalaler <dalalaler@gmail.com>
2019-09-02 21:09:08 +0300

    Renamed a to variant

--- homeworks/daniil_volk/lesson1/hello_world.py
+++ homeworks/daniil_volk/lesson1/hello_world.py
@@ -3,19 +3,20 @@ import webbrowser
 import os
 
 
-a = verification()
-if a == 1:
+print('Enter variant:')
+variant = verification(1,7)
+if variant == 1:
     print("Hello world!")
-if a == 2:
+if variant == 2:
     print("My name is Danil, i'm 22 old, and i wanna learn python.")
-if a == 3:
+if variant == 3:
     print(os.name)
-if a == 4:
+if variant == 4:
     print("Enter your name :")
     b = str(input())
     print("Hello,", b, "!")
-if a == 5:
+if variant == 5:
     print("Change a variant (1-6)")
-    a = int(input())
-if a == 6:
+    variant = int(input())
+if variant == 6:
     webbrowser.open("https://youtu.be/dQw4w9WgXcQ")
