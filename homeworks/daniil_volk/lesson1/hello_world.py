2a0569c302974236247e9a5ad40a5bffd8a9ab91
Dalalaler <dalalaler@gmail.com>
2019-09-02 19:45:10 +0300

    Fixed hello world

--- homeworks/daniil_volk/lesson1/hello_world.py
+++ homeworks/daniil_volk/lesson1/hello_world.py
@@ -1,12 +1,14 @@
-print("Change a variant (1-6)")
-a = int(input())
+from verification import verification
+import webbrowser
+import os
+
+
+a = verification()
 if a == 1:
     print("Hello world!")
 if a == 2:
     print("My name is Danil, i'm 22 old, and i wanna learn python.")
 if a == 3:
-    import os
-
     print(os.name)
 if a == 4:
     print("Enter your name:")
@@ -16,7 +18,5 @@ if a == 5:
     print("Change a variant (1-6)")
     a = int(input())
 if a == 6:
-    import webbrowser
-
     webbrowser.open("https://youtu.be/dQw4w9WgXcQ")
     print("Bye world!")
