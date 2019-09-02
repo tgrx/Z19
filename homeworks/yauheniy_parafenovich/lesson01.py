ab98cda9ee8775faabf310759fc00d45961f3da1
Evgen1177 <g5577@tut.by>
2019-09-02 20:17:26 +0300

    kkk

--- homeworks/yauheniy_parafenovich/lesson01.py
+++ homeworks/yauheniy_parafenovich/lesson01.py
@@ -1,11 +1,9 @@
-import os
-
 print("Hello World\nI am Lenovo")
-print("I Work on a systeme " + os.name)
+print(f"I Work on a systeme")
 name = input("What you name?\n")
-print("Hello " + name)
-i = input("do you like programming\n")
-if i == "yes":  # проверка условия
+print(f"Hello, {name}")
+answer = input("do you like programming\n")
+if answer.lower() == "yes":  # проверка условия
     print("good")
 else:
     print("bad")
--- homeworks/yauheniy_parafenovich/lesson05.py
+++ homeworks/yauheniy_parafenovich/lesson05.py
@@ -1,6 +1,26 @@
-def reversed2(L):
-    x = list([])
-    for s in L:
-        x.append(s)
-        z = x[::-1]
-    print(z)  # функция реверса
+# def reversed2(L):
+#    x = list([])
+#   for s in L:
+#      x.append(s)
+#     z = x[::-1]
+# print(z)  # функция реверса
+
+
+def sorted(a):
+    n = len(a)
+    count = 0
+    b = [1]
+    i = 1
+    while count < n:
+        if a[0] > a[i]:
+            b[0] = a[0]
+            print(b)
+        else:
+            print(a)
+        count = count + 1
+    i = i + 1
+
+
+# sorted((13, 6, 5, 4))
+
+sorted((5, 3, 4, 2, 1))
