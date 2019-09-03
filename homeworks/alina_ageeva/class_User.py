b413fe699b91c3bd3c1aefb4a66add8340f7d332
OswinOswald <linaa_2017@bk.ru>
2019-09-03 22:13:49 +0300

    Создание класса User

---
+++ homeworks/alina_ageeva/class_User.py
@@ -0,0 +1,14 @@
+class User:
+    def __repr__(self):
+        return f"User(name = {self.name}, email = {self.email})"
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __eq__(self, other: "User"):
+        return self.email == other.email
+
+u1 = User("1", "@")
+u2 = User("1", "@")
+print(u1 == u2)
