65c6ef7979fdf3f249a4d81f974a8afacf1e5468
kefir331 <Seven1486>
2019-09-03 18:34:18 +0300

    Merge branch 'master' of D:\z19 with conflicts.

--- homeworks/pavel_titov/lesson05.py
+++ homeworks/pavel_titov/lesson05.py
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
+=======
+## Уровень 2
+
+
+def Reversed(x):
+    if x == ([], (), str):
+        return(reversed(x))
+    else:
+        print("Error")
+
+
+## Уровень 3
+>>>>>>> Merge branch 'master' of D:\z19 with conflicts.
 
