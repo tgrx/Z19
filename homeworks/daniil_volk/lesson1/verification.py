996ccb3b387f80af632e2918e833f86267577d4c
Dalalaler <dalalaler@gmail.com>
2019-09-04 17:05:56 +0300

    Fixed verification

--- homeworks/daniil_volk/lesson1/verification.py
+++ homeworks/daniil_volk/lesson1/verification.py
@@ -3,10 +3,7 @@ def verification(start, ending):
         try:
             a = int(input())
         except ValueError:
-            print("Variant is not an integer!")
             continue
         else:
             if a in range(start, ending + 1):
-                print("Variant is available!")
                 return a
-            print(f"Variant is not {start} - {ending}!")
