7a160d36eadc06e92f9c0ee8a46aa536367a946b
Lizaveta <54038052+lizaveta-stasevich@users.noreply.github.com>
2019-09-05 00:02:53 +0300

    les 3, lev 4 (#82)
    * les 3, lev 4
    
    * les 3, lev 5
    
    * les 3, lev 6
    
    * les 3, lev 6

---
+++ homeworks/lizaveta_stasevich/les_3_lev_5.py
@@ -0,0 +1,28 @@
+def enqueue(l, e):
+    """ добавляет элемент в конец очереди """
+    s = l.append(e)
+    l = s
+    return l
+
+
+def dequeue(l):
+    """
+    вынимает элемент из начала очереди и возвращает его
+    если очередь пуста - возвращается None
+    """
+    if not l:
+        return None
+    return l.pop(0)
+
+
+if __name__ == "__main__":
+    x = []
+    assert dequeue(x) is None
+    assert enqueue(x, 1) is None
+    assert enqueue(x, 2) is None
+    assert dequeue(x) == 1
+    assert enqueue(x, 3) is None
+    assert dequeue(x) == 2
+    assert dequeue(x) == 3
+    assert dequeue(x) is None
+    assert x == []
