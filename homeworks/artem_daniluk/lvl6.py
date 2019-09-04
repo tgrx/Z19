0de7eee79dafe9d7b261c0de890cb89bb89fae78
daniluuuuuuuk <54038074+daniluuuuuuuk@users.noreply.github.com>
2019-09-05 00:53:19 +0300

    Artem Danilyuk ackerman function

---
+++ homeworks/artem_daniluk/lvl6.py
@@ -0,0 +1,38 @@
+def enqueue(l, p, e):
+    """ добавляет элемент e в конец очереди l с приоритетом p """
+    return l.setdefault(p, []).append(e)
+
+
+def dequeue(l):
+    """
+    вынимает элемент из начала очереди и возвращает его
+    если очередь пуста - возвращается None
+    элементы возвращаются согласно приоритету
+    """
+    for p in reversed(sorted(l)):
+        q = l[p]
+        if not q:
+            del l[p]
+            continue
+        return q.pop(0)
+
+
+if __name__ == "__main__":
+    x = {}
+    assert dequeue(x) is None
+    assert enqueue(x, 1, "a") is None
+    assert enqueue(x, 1, "b") is None
+    assert enqueue(x, 2, "aa") is None
+    assert dequeue(x) == "aa"
+    assert enqueue(x, 1, "c") is None
+    assert enqueue(x, 3, "aaa") is None
+    assert enqueue(x, 3, "bbb") is None
+    assert enqueue(x, 2, "bb") is None
+    assert dequeue(x) == "aaa"
+    assert dequeue(x) == "bbb"
+    assert dequeue(x) == "bb"
+    assert dequeue(x) == "a"
+    assert dequeue(x) == "b"
+    assert dequeue(x) == "c"
+    assert dequeue(x) is None
+    assert x == {}
