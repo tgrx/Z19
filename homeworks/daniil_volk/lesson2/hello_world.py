63a310dc4721993106e3574f29cd970b13d26f52
Dalalaler <dalalaler@gmail.com>
2019-09-04 17:56:59 +0300

    Added import in lesson2

--- homeworks/daniil_volk/lesson2/hello_world.py
+++ homeworks/daniil_volk/lesson2/hello_world.py
@@ -1,49 +1,25 @@
-import os
 import webbrowser
+import os
+import sys
 
-arr = [1, 2, 3, 4, 5, 6]
+sys.path.append("..")
+from lesson1.verification import verification
 
-while True:  # Выполняем бесконечно, пока не встретим break
-    try:
-        a = int(input("Enter your variant (1-6): "))
-    except ValueError:  # Выполняем проверку на тип int,
-        # Делаем через искючения exceptions, ValueError - функция получает аргумент правильного типа, но некорректного значения.
-        print("Variant is not an integer!")
-        continue
-    else:
-        if a in arr:  # Вместо банального сравнения решил поиграться со списками
-            print("Variant is available!")
-            break
-        else:
-            print("Variant is not 1-6!")
 
-if a == 1:
+print("Enter variant:")
+variant = verification(1, 6)
+if variant == 1:
     print("Hello world!")
-if a == 2:
+if variant == 2:
     print("My name is Danil, i'm 22 old, and i wanna learn python.")
-if a == 3:
-
+if variant == 3:
     print(os.name)
-if a == 4:
-    print("Enter your name:")
+if variant == 4:
+    print("Enter your name :")
     b = str(input())
-    b = "Hello, " + b + "!"  # Соединение строк
-    print(b)
-if a == 5:
-
-    while True:  # Выполняем бесконечно, пока не встретим break
-        try:
-            a = int(input("Enter your variant (1-6): "))
-        except ValueError:  # Выполняем проверку на тип int,
-            # Делаем через искючения exceptions, ValueError - функция получает аргумент правильного типа, но некорректного значения.
-            print("Variant is not an integer!")
-            continue
-        else:
-            if a in arr:  # Вместо банального сравнения решил поиграться со списками
-                print("Variant is available!")
-                break
-            else:
-                print("Variant is not 1-6!")
-
-if a == 6:
+    print("Hello,", b, "!")
+if variant == 5:
+    print("Change a variant (1-6)")
+    variant = int(input())
+if variant == 6:
     webbrowser.open("https://youtu.be/dQw4w9WgXcQ")
