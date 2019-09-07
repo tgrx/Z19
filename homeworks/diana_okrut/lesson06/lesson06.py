b7be66729aa32fb2eb9be91e1502938278242177
Diana Okrut <zyndel94@gmail.com>
2019-09-07 19:30:31 +0300

    les06

--- homeworks/diana_okrut/lesson06/lesson06.py
+++ homeworks/diana_okrut/lesson06/lesson06.py
@@ -1,37 +1,363 @@
-# class User:
-#     def __init__(self, name, email):
-#         self.name = name
-#         self.email = email
-#
-#     def __eq__(self, other):
-#         return self.email == other.email
-#
-#
-# class UserForm(User):
-#     def validate_name(self):
-#         a = ord(self.name[0])
-#         b = ord(self.name[-1])
-#         c = ord(self.name[1:-2])
-#         if (not 97 <= a <= 122) and (a != 95):
-#             return ValueError
-#         if (not 97 <= b <= 122) and (not 48 <= b <= 57) and (b != 95):
-#             return ValueError
-#         if (not 97 <= c <= 122) and (not 48 <= c <= 57) and (c != 95) and (c != 46):
-#             return ValueError
-#         else:
-#             return self
-#
-#     def validate_email(self):
-#         x = self.email.split('@')
-#         y = x[0]
-#         z = x[1]
-#         if (not 97 <= ord(y[0]), ord(z[0]) <= 122) and (ord(y[0]) != 95) and (ord(z[0]) != 95):
-#             return ValueError
-#         if (not 97 <= ord(y[-1]), ord(z[-1]) <= 122) and (not 48 <= ord(y[-1]), ord(z[-1]) <= 57) and (
-#                 ord(y[-1]) != 95) and (ord(z[-1]) != 95):
-#             return ValueError
-#         if (not 97 <= (y[1:-2]), (z[1:-2]) <= 122) and (not 48 <= (y[1:-2]), (z[1:-2]) <= 57) and (
-#                 (y[1:-2]) not in [95, 46, 43, ]) and ((z[1:-2]) not in [95, 46, ]):
-#             return ValueError
-#         else:
-#             return self
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __eq__(self, other):
+        return self.email == other.email
+
+
+class UserForm(User):
+    def validate_name(self):
+        if self.name[0] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+        ]:
+            return ValueError
+        if self.name[-1] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        ]:
+            return ValueError
+        lisname = list(self.name).pop(0)
+        lisname = lisname.pop(-1)
+        for x in lisname:
+            if x not in [
+                "_",
+                ".",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "q",
+                "w",
+                "r",
+                "t",
+                "y",
+                "u",
+                "i",
+                "o",
+                "p",
+                "s",
+                "h",
+                "j",
+                "k",
+                "l",
+                "z",
+                "x",
+                "v",
+                "n",
+                "m",
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            ]:
+                return ValueError
+        else:
+            return self
+
+    def validate_email(self):
+        lisemail = self.email.split("@")
+        login = lisemail[0]
+        domain = lisemail[1]
+        if login[0] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+        ]:
+            return ValueError
+        if login[-1] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        ]:
+            return ValueError
+        lisname = list(login).pop(0)
+        lisname = lisname.pop(-1)
+        for x in lisname:
+            if x not in [
+                "_",
+                ".",
+                "+",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "q",
+                "w",
+                "r",
+                "t",
+                "y",
+                "u",
+                "i",
+                "o",
+                "p",
+                "s",
+                "h",
+                "j",
+                "k",
+                "l",
+                "z",
+                "x",
+                "v",
+                "n",
+                "m",
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            ]:
+                return ValueError
+        if domain[0] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+        ]:
+            return ValueError
+        if domain[-1] not in [
+            "_",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "q",
+            "w",
+            "r",
+            "t",
+            "y",
+            "u",
+            "i",
+            "o",
+            "p",
+            "s",
+            "h",
+            "j",
+            "k",
+            "l",
+            "z",
+            "x",
+            "v",
+            "n",
+            "m",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        ]:
+            return ValueError
+        lisname = list(domain).pop(0)
+        lisname = lisname.pop(-1)
+        for x in lisname:
+            if x not in [
+                "_",
+                ".",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "q",
+                "w",
+                "r",
+                "t",
+                "y",
+                "u",
+                "i",
+                "o",
+                "p",
+                "s",
+                "h",
+                "j",
+                "k",
+                "l",
+                "z",
+                "x",
+                "v",
+                "n",
+                "m",
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            ]:
+                return ValueError
+        else:
+            return self
