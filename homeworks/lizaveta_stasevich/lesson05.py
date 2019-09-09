72263b2d47f8112c7699dd699ffc6987a35aca04
Liza <lizik.stasevich@gmail.com>
2019-09-09 21:17:44 +0300

    range

--- homeworks/lizaveta_stasevich/lesson05.py
+++ homeworks/lizaveta_stasevich/lesson05.py
@@ -48,3 +48,44 @@ def LazyReversed(x):
     for i in range(len(x) - 1, -1, -1):
         rev_x.append(x[i])
     return iter(rev_x)
+
+
+class Range:
+    def __init__(self, *args):
+        if not args:
+            raise TypeError("Enter stop")
+        if len(args) == 2:
+            self.step = 1
+            self.start = args[0]
+            self.stop = args[1]
+            self.current = args[0]
+        if len(args) == 1:
+            self.stop = args[0]
+            self.start = 0
+            self.step = 1
+            self.current = 0
+        if len(args) == 3:
+            self.start = args[0]
+            self.stop = args[1]
+            self.step = args[2]
+            self.current = args[0]
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.stop >= self.start and self.step < 0:
+            raise StopIteration
+        if self.stop <= self.start and self.step > 0:
+            raise StopIteration
+        if self.start < self.stop:
+            if self.current >= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
+        elif self.start > self.stop:
+            if self.current <= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
