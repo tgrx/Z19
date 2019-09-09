ce50b7a84be2c88b84e1bc6dc5a035b5a64d63c1
Evgen1177 <54038274+Evgen1177@users.noreply.github.com>
2019-09-09 19:16:23 +0300

    lesson3.4 (#95)
    *  lesson3.4
    
    *  lesson3.2
    
    *  lesson3.5, 3.6
    
    *  blacked!

---
+++ homeworks/yauheniy_parafenovich/lesson3.4.py
@@ -0,0 +1,8 @@
+def ackermann(m, n):
+    """функция Аккермана"""
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    elif m > 0 and n > 0:
+        return ackermann(m - 1, ackermann(m, n - 1))
