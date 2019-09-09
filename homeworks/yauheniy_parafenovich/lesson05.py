e9b09cf266407484681ff7ba0ef8b177b6ac70fc
Evgen1177 <g5577@tut.by>
2019-09-09 20:30:55 +0300

    Reversed

--- homeworks/yauheniy_parafenovich/lesson05.py
+++ homeworks/yauheniy_parafenovich/lesson05.py
@@ -1,5 +1,10 @@
-def Reversed(L):
- z = L[::-1]
- print(z)
+#def Reversed(L):
+ #z = L[::-1]
+ #print(z)
 
 
+def reverse_string2(s):
+    return "".join(reversed(s))
+
+data = reverse_string2([])
+print(data)
