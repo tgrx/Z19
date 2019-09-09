1c2f8b1c9c7f0c8b4045b4efc2febcd686be4b78
Diana Okrut <zyndel94@gmail.com>
2019-09-09 21:32:11 +0300

    UserForm

--- homeworks/diana_okrut/lesson06/lesson06.py
+++ homeworks/diana_okrut/lesson06/lesson06.py
@@ -31,23 +31,20 @@ class UserForm(User):
         b = "qwertyuiopasdfghjklzxcvbnm0123456789_"
         c = "qwertyuiopasdfghjklzxcvbnm0123456789_."
         d = "qwertyuiopasdfghjklzxcvbnm0123456789_+."
-        try:
-            lisemail = self.email.split("@")
-            login = lisemail[0]
-            domain = lisemail[1]
-            if login[0] not in a:
+        lisemail = self.email.split("@")
+        login = lisemail[0]
+        domain = lisemail[1]
+        if login[0] not in a:
+            raise ValueError
+        if login[-1] not in b:
+            raise ValueError
+        for x in login[1, -1]:
+            if x not in d:
                 raise ValueError
-            if login[-1] not in b:
+        if domain[0] not in a:
+            raise ValueError
+        if domain[-1] not in b:
+            raise ValueError
+        for x in domain[1, -1]:
+            if x not in c:
                 raise ValueError
-            for x in login[1, -1]:
-                if x not in d:
-                    raise ValueError
-            if domain[0] not in a:
-                raise ValueError
-            if domain[-1] not in b:
-                raise ValueError
-            for x in domain[1, -1]:
-                if x not in c:
-                    raise ValueError
-        finally:
-            None
