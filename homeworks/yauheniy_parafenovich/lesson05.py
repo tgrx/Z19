50fa7635326cf7551ee92d7c04ef5c53012c4165
Evgen1177 <g5577@tut.by>
2019-09-09 21:24:59 +0300

    Reversed

--- homeworks/yauheniy_parafenovich/lesson05.py
+++ homeworks/yauheniy_parafenovich/lesson05.py
@@ -1,11 +1,8 @@
-# def Reversed(L):
-# z = L[::-1]
-# print(z)
-
-
-def reverse_string2(s):
-    return "".join(reversed(s))
-
-
-data = reverse_string2([])
-print(data)
+def Reversed(y):
+    new = ""
+    index = len(y)
+    while index:
+        index = index -1
+        new = new + y[index]
+    return new
+print(Reversed("саша"))
