0f42b3f862b46ea01c25fd9f7988a4ca3d910e52
Dalalaler <dalalaler@gmail.com>
2019-09-09 21:58:01 +0300

    Blacked

--- homeworks/daniil_volk/lesson5/lesson05.py
+++ homeworks/daniil_volk/lesson5/lesson05.py
@@ -21,27 +21,27 @@ def Sorted(x):
     return x
 
 
-# def Filter(y, x):
-#     x = list(x)
-#     l = len(x)
-#     z = []
-#     if isinstance(y, bool):
-#         if y is True:
-#             for i in range(l):
-#                 if x[i] > 0:
-#                     z.append(x[i])
-#             return z
-#         else:
-#             for i in range(l):
-#                 if x[i] == 0:
-#                     z.append(x[i])
-#             return z
-#     else:
-#         y = str(y)
-#         for i in range(l):
-#             if y in x[i]:
-#                 z.append(x[i])
-#         return z
+def Filter(y, x):
+    x = list(x)
+    l = len(x)
+    z = []
+    if isinstance(y, bool):
+        if y is True:
+            for i in range(l):
+                if x[i] > 0:
+                    z.append(x[i])
+            return z
+        else:
+            for i in range(l):
+                if x[i] == 0:
+                    z.append(x[i])
+            return z
+    else:
+        y = str(y)
+        for i in range(l):
+            if y in x[i]:
+                z.append(x[i])
+        return z
 
 
 def TypedReversed(x):
@@ -72,81 +72,81 @@ def TypedReversed(x):
         return str(x)
 
 
-#
-#
-# class Range:
-#     def __init__(self, start, stop="default", step="default"):
-#         if step == "default":
-#             self.step = 1
-#             self.start = start
-#             self.stop = stop
-#             self.current = start
-#         if stop == "default":
-#             self.stop = start
-#             self.start = 0
-#             self.step = 1
-#             self.current = start
-#         if stop != "default" and step != "default":
-#             self.start = start
-#             self.stop = stop
-#             self.step = step
-#             self.current = start
-#
-#     def __iter__(self):
-#         self.current = self.start
-#         return self
-#
-#     def __next__(self):
-#         if self.stop >= self.start and self.step < 0:
-#             raise StopIteration
-#         if self.stop <= self.start and self.step > 0:
-#             raise StopIteration
-#         if self.start < self.stop:
-#             if self.current >= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + self.step
-#             return c
-#         elif self.start > self.stop:
-#             if self.current <= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + self.step
-#             return c
-#
-#
-# class DateRange:
-#     def __init__(self, start, stop="default", step="default"):
-#         if step == "default":
-#             self.step = 1
-#             self.start = start
-#             self.stop = stop
-#             self.current = start
-#         if stop == "default":
-#             self.stop = start
-#             self.start = 0
-#             self.step = 1
-#             self.current = start
-#         if stop != "default" and step != "default":
-#             self.start = start
-#             self.stop = stop
-#             self.step = step
-#             self.current = start
-#
-#     def __iter__(self):
-#         self.current = self.start
-#         return self
-#
-#     def __next__(self):
-#         if self.stop >= self.start and self.step < 0:
-#             raise StopIteration
-#         if self.stop <= self.start and self.step > 0:
-#             raise StopIteration
-#         if self.start < self.stop:
-#             if self.current >= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + t(self.step)
-#             return c
-#         elif self.start > self.stop:
-#             if self.current <= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + t(self.step)
-#             return c
+
+
+class Range:
+    def __init__(self, start, stop="default", step="default"):
+        if step == "default":
+            self.step = 1
+            self.start = start
+            self.stop = stop
+            self.current = start
+        if stop == "default":
+            self.stop = start
+            self.start = 0
+            self.step = 1
+            self.current = start
+        if stop != "default" and step != "default":
+            self.start = start
+            self.stop = stop
+            self.step = step
+            self.current = start
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.stop >= self.start and self.step < 0:
+            raise StopIteration
+        if self.stop <= self.start and self.step > 0:
+            raise StopIteration
+        if self.start < self.stop:
+            if self.current >= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
+        elif self.start > self.stop:
+            if self.current <= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
+
+
+class DateRange:
+    def __init__(self, start, stop="default", step="default"):
+        if step == "default":
+            self.step = 1
+            self.start = start
+            self.stop = stop
+            self.current = start
+        if stop == "default":
+            self.stop = start
+            self.start = 0
+            self.step = 1
+            self.current = start
+        if stop != "default" and step != "default":
+            self.start = start
+            self.stop = stop
+            self.step = step
+            self.current = start
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.stop >= self.start and self.step < 0:
+            raise StopIteration
+        if self.stop <= self.start and self.step > 0:
+            raise StopIteration
+        if self.start < self.stop:
+            if self.current >= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + t(self.step)
+            return c
+        elif self.start > self.stop:
+            if self.current <= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + t(self.step)
+            return c
