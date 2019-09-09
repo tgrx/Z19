8ebde955a47aebeb6e520af24e2e2e478f4b5029
Diana Okrut <zyndel94@gmail.com>
2019-09-09 12:53:56 +0300

    /

--- homeworks/diana_okrut/lesson06/lesson06.py
+++ homeworks/diana_okrut/lesson06/lesson06.py
@@ -1,10 +1,10 @@
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __eq__(self, other):
-        return self.email == other.email
+# class User:
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
+#
+#     def __eq__(self, other):
+#         return self.email == other.email
 
 
 # class UserForm(User):
