0a2b031e7ab04b21f75ae6da69b74e51e7b25020
Evgen1177 <g5577@tut.by>
2019-09-09 21:27:51 +0300

    Reversed black

--- homeworks/yauheniy_parafenovich/lesson05.py
+++ homeworks/yauheniy_parafenovich/lesson05.py
@@ -2,7 +2,6 @@ def Reversed(y):
     new = ""
     index = len(y)
     while index:
-        index = index -1
+        index = index - 1
         new = new + y[index]
     return new
-print(Reversed("саша"))
