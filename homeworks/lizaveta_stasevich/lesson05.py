d910b7d2c6f91965b5e2507d886d8b42c5b0db5c
Liza <lizik.stasevich@gmail.com>
2019-09-09 20:46:52 +0300

    До lazyRev

---
+++ homeworks/lizaveta_stasevich/lesson05.py
@@ -0,0 +1,50 @@
+import math
+
+
+def Reversed(x):
+    x = list(x)
+    rev_x = []
+    for i in range(len(x) - 1, -1, -1):
+        rev_x.append(x[i])
+    return rev_x
+
+
+def Sorted(x):
+    r = list(x)
+    for i in range(len(x) - 1):
+        for j in range(len(x) - i - 1):
+            if r[j] > r[j + 1]:
+                t = r[j]
+                r[j] = r[j + 1]
+                r[j + 1] = t
+    return r
+
+
+def Filter(p, x):
+    x = list(x)
+    r = []
+    for i in range(len(x)):
+        if p(x[i]):
+            r.append(x[i])
+    return r
+
+
+def TypedReversed(x):
+    t = type(x)
+    x = list(x)
+    rev_x = []
+    for i in range(len(x) - 1, -1, -1):
+        rev_x.append(x[i])
+    if t == str:
+        rev_x = "".join(rev_x)
+    else:
+        rev_x = t(rev_x)
+    return rev_x
+
+
+def LazyReversed(x):
+    x = list(x)
+    rev_x = []
+    for i in range(len(x) - 1, -1, -1):
+        rev_x.append(x[i])
+    return iter(rev_x)
