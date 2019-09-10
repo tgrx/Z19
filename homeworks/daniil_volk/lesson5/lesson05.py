c91a7ebd285d01554870fc85f181d7bba1fbb3d4
Dalalaler <dalalaler@gmail.com>
2019-09-10 19:23:48 +0300

    Rebuilded Range

--- homeworks/daniil_volk/lesson5/lesson05.py
+++ homeworks/daniil_volk/lesson5/lesson05.py
@@ -60,48 +60,56 @@ def LazyReversed(x):
     return iter(x)
 
 
+class Range:
+    def __init__(self, start, stop="default", step="default"):
+        if step == "default":
+            self.step = 1
+            self.start = start
+            self.stop = stop
+            self.current = start
+        if stop == "default":
+            self.stop = start
+            self.start = 0
+            self.step = 1
+            self.current = start
+        if stop != "default" and step != "default":
+            self.start = start
+            self.stop = stop
+            self.step = step
+            self.current = start
+        check = self.start
+        if not isinstance(check, (str, tuple, list)):
+            return
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.stop >= self.start and self.step < 0:
+            raise StopIteration
+        if self.stop <= self.start and self.step > 0:
+            raise StopIteration
+        if self.start < self.stop:
+            if self.current >= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
+        elif self.start > self.stop:
+            if self.current <= self.stop:
+                raise StopIteration
+            c, self.current = self.current, self.current + self.step
+            return c
+
+
 #
 #
-#
-#
-# class Range:
-#     def __init__(self, start, stop="default", step="default"):
-#         if step == "default":
-#             self.step = 1
-#             self.start = start
-#             self.stop = stop
-#             self.current = start
-#         if stop == "default":
-#             self.stop = start
-#             self.start = 0
-#             self.step = 1
-#             self.current = start
-#         if stop != "default" and step != "default":
-#             self.start = start
-#             self.stop = stop
-#             self.step = step
-#             self.current = start
-#
-#     def __iter__(self):
-#         self.current = self.start
-#         return self
-#
-#     def __next__(self):
-#         if self.stop >= self.start and self.step < 0:
-#             raise StopIteration
-#         if self.stop <= self.start and self.step > 0:
-#             raise StopIteration
-#         if self.start < self.stop:
-#             if self.current >= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + self.step
-#             return c
-#         elif self.start > self.stop:
-#             if self.current <= self.stop:
-#                 raise StopIteration
-#             c, self.current = self.current, self.current + self.step
-#             return c
-#
+# print(list(Range(3)) == [0, 1, 2])
+# print(tuple(Range(-1, 2)) == (-1, 0, 1))
+# print(tuple(Range(0, -10, -2)) == (0, -2, -4, -6, -8))
+# print(list(Range(0, -10, 3)) == [])
+# print(list(Range(10, -10, -1)))
+# print(list(Range(-10, 10, 1)))
 #
 # class DateRange:
 #     def __init__(self, start, stop="default", step="default"):
