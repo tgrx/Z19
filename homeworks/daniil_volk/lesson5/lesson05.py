93d6d6693d0249455213ceb37479f6ea25f75ed6
Dalalaler <dalalaler@gmail.com>
2019-09-10 17:06:26 +0300

    Tested

--- homeworks/daniil_volk/lesson5/lesson05.py
+++ homeworks/daniil_volk/lesson5/lesson05.py
@@ -44,109 +44,109 @@ def Filter(y, x):
         return z
 
 
-def TypedReversed(x):
-    typex = type(x)
-    print(typex)
-    if typex == dict:
-        raise TypeError
-    x = list(x)
-    l = len(x)
-    steps = l // 2
-    for i in range(steps):
-        l -= 1
-        x[i], x[l] = x[l], x[i]
-    if typex == set:
-        return set(x)
-    elif typex == tuple:
-        return tuple(x)
-    elif typex == list:
-        return list(x)
-    elif typex == frozenset:
-        return frozenset(x)
-    elif typex == str:
-        x = str(x)
-        x = x.replace("'", "")
-        x = x.replace(", ", "")
-        x = x.replace("[", "")
-        x = x.replace("]", "")
-        return str(x)
-
-
-
-
-class Range:
-    def __init__(self, start, stop="default", step="default"):
-        if step == "default":
-            self.step = 1
-            self.start = start
-            self.stop = stop
-            self.current = start
-        if stop == "default":
-            self.stop = start
-            self.start = 0
-            self.step = 1
-            self.current = start
-        if stop != "default" and step != "default":
-            self.start = start
-            self.stop = stop
-            self.step = step
-            self.current = start
-
-    def __iter__(self):
-        self.current = self.start
-        return self
-
-    def __next__(self):
-        if self.stop >= self.start and self.step < 0:
-            raise StopIteration
-        if self.stop <= self.start and self.step > 0:
-            raise StopIteration
-        if self.start < self.stop:
-            if self.current >= self.stop:
-                raise StopIteration
-            c, self.current = self.current, self.current + self.step
-            return c
-        elif self.start > self.stop:
-            if self.current <= self.stop:
-                raise StopIteration
-            c, self.current = self.current, self.current + self.step
-            return c
-
-
-class DateRange:
-    def __init__(self, start, stop="default", step="default"):
-        if step == "default":
-            self.step = 1
-            self.start = start
-            self.stop = stop
-            self.current = start
-        if stop == "default":
-            self.stop = start
-            self.start = 0
-            self.step = 1
-            self.current = start
-        if stop != "default" and step != "default":
-            self.start = start
-            self.stop = stop
-            self.step = step
-            self.current = start
-
-    def __iter__(self):
-        self.current = self.start
-        return self
-
-    def __next__(self):
-        if self.stop >= self.start and self.step < 0:
-            raise StopIteration
-        if self.stop <= self.start and self.step > 0:
-            raise StopIteration
-        if self.start < self.stop:
-            if self.current >= self.stop:
-                raise StopIteration
-            c, self.current = self.current, self.current + t(self.step)
-            return c
-        elif self.start > self.stop:
-            if self.current <= self.stop:
-                raise StopIteration
-            c, self.current = self.current, self.current + t(self.step)
-            return c
+# def TypedReversed(x):
+#     typex = type(x)
+#     print(typex)
+#     if typex == dict:
+#         raise TypeError
+#     x = list(x)
+#     l = len(x)
+#     steps = l // 2
+#     for i in range(steps):
+#         l -= 1
+#         x[i], x[l] = x[l], x[i]
+#     if typex == set:
+#         return set(x)
+#     elif typex == tuple:
+#         return tuple(x)
+#     elif typex == list:
+#         return list(x)
+#     elif typex == frozenset:
+#         return frozenset(x)
+#     elif typex == str:
+#         x = str(x)
+#         x = x.replace("'", "")
+#         x = x.replace(", ", "")
+#         x = x.replace("[", "")
+#         x = x.replace("]", "")
+#         return str(x)
+#
+#
+#
+#
+# class Range:
+#     def __init__(self, start, stop="default", step="default"):
+#         if step == "default":
+#             self.step = 1
+#             self.start = start
+#             self.stop = stop
+#             self.current = start
+#         if stop == "default":
+#             self.stop = start
+#             self.start = 0
+#             self.step = 1
+#             self.current = start
+#         if stop != "default" and step != "default":
+#             self.start = start
+#             self.stop = stop
+#             self.step = step
+#             self.current = start
+#
+#     def __iter__(self):
+#         self.current = self.start
+#         return self
+#
+#     def __next__(self):
+#         if self.stop >= self.start and self.step < 0:
+#             raise StopIteration
+#         if self.stop <= self.start and self.step > 0:
+#             raise StopIteration
+#         if self.start < self.stop:
+#             if self.current >= self.stop:
+#                 raise StopIteration
+#             c, self.current = self.current, self.current + self.step
+#             return c
+#         elif self.start > self.stop:
+#             if self.current <= self.stop:
+#                 raise StopIteration
+#             c, self.current = self.current, self.current + self.step
+#             return c
+#
+#
+# class DateRange:
+#     def __init__(self, start, stop="default", step="default"):
+#         if step == "default":
+#             self.step = 1
+#             self.start = start
+#             self.stop = stop
+#             self.current = start
+#         if stop == "default":
+#             self.stop = start
+#             self.start = 0
+#             self.step = 1
+#             self.current = start
+#         if stop != "default" and step != "default":
+#             self.start = start
+#             self.stop = stop
+#             self.step = step
+#             self.current = start
+#
+#     def __iter__(self):
+#         self.current = self.start
+#         return self
+#
+#     def __next__(self):
+#         if self.stop >= self.start and self.step < 0:
+#             raise StopIteration
+#         if self.stop <= self.start and self.step > 0:
+#             raise StopIteration
+#         if self.start < self.stop:
+#             if self.current >= self.stop:
+#                 raise StopIteration
+#             c, self.current = self.current, self.current + t(self.step)
+#             return c
+#         elif self.start > self.stop:
+#             if self.current <= self.stop:
+#                 raise StopIteration
+#             c, self.current = self.current, self.current + t(self.step)
+#             return c
