c1b51d6b2e3c365639615921c5e273292a4635c5
Z1909 <getfun.net@gmail.com>
2019-09-11 19:52:42 +0300

    lesson 5: filter, sorted, reversed funcs

--- homeworks/oleg_vasilchik/lesson5/lesson05.py
+++ homeworks/oleg_vasilchik/lesson5/lesson05.py
@@ -7,3 +7,21 @@ def Reversed(x):
         index -= 1
         new += [x[index]]
     return new
+
+
+def Sorted(x):
+    x = list(x)
+    for j in range(len(x) - 1):
+        for i in range(len(x) - j - 1):
+            if x[i] > x[i + 1]:
+                x[i], x[i + 1] = x[i + 1], x[i]
+    print(x)
+
+
+def Filter(x, y):
+    y = list(y)
+    new = []
+    for i in range(len(y)):
+        if x is y[i]:
+            new.append(x)
+    return new
