40c04b17e5287946af1fa89f38d0b052f04972d0
Dalalaler <dalalaler@gmail.com>
2019-09-11 19:06:18 +0300

    Added validation

--- homeworks/daniil_volk/lesson6/lesson06.py
+++ homeworks/daniil_volk/lesson6/lesson06.py
@@ -8,45 +8,45 @@ class User:
             return self.email == other.email
 
 
-# class UserForm(User):
-#     def validate_name(self):
-#         if isinstance(self.name[0], int):
-#             return ValueError
-#         if self.name[0] == '.' or self.name[-1] == '.':
-#             return ValueError
-#         a = (not any(c.islower() for c in self.name))
-#         b = (not self.name.isnumeric())
-#         c = (self.name != '_')
-#         d = ('.' not in self.name)
-#         if a and b and c and d:
-#             return ValueError
-#
-#     def validate_email(self):
-#         em = self.email.split("@")
-#         log = em[0]
-#         dom = em[1]
-#
-#         if log[0] == '+' or log[-1] == '+':
-#             return ValueError
-#         if isinstance(log[0], int):
-#             return ValueError
-#         if log[0] == '.' or log[-1] == '.':
-#             return ValueError
-#         a = (not any(c.islower() for c in log))
-#         b = (not log.isnumeric())
-#         c = (log != '_')
-#         d = ('.' not in log)
-#         e = ('+' not in log)
-#         if a and b and c and d and e:
-#             return ValueError
-#
-#         if isinstance(dom[0], int):
-#             return ValueError
-#         if dom[0] == '.' or dom[-1] == '.':
-#             return ValueError
-#         a = (not any(c.islower() for c in dom))
-#         b = (not dom.isnumeric())
-#         c = (dom != '_')
-#         d = ('.' not in dom)
-#         if a and b and c and d:
-#             return ValueError
+class UserForm(User):
+    def validate_name(self):
+        if isinstance(self.name[0], int):
+            return ValueError
+        if self.name[0] == '.' or self.name[-1] == '.':
+            return ValueError
+        a = (not any(c.islower() for c in self.name))
+        b = (not self.name.isnumeric())
+        c = (self.name != '_')
+        d = ('.' not in self.name)
+        if a and b and c and d:
+            return ValueError
+
+    def validate_email(self):
+        em = self.email.split("@")
+        log = em[0]
+        dom = em[1]
+
+        if log[0] == '+' or log[-1] == '+':
+            return ValueError
+        if isinstance(log[0], int):
+            return ValueError
+        if log[0] == '.' or log[-1] == '.':
+            return ValueError
+        a = (not any(c.islower() for c in log))
+        b = (not log.isnumeric())
+        c = (log != '_')
+        d = ('.' not in log)
+        e = ('+' not in log)
+        if a and b and c and d and e:
+            return ValueError
+
+        if isinstance(dom[0], int):
+            return ValueError
+        if dom[0] == '.' or dom[-1] == '.':
+            return ValueError
+        a = (not any(c.islower() for c in dom))
+        b = (not dom.isnumeric())
+        c = (dom != '_')
+        d = ('.' not in dom)
+        if a and b and c and d:
+            return ValueError
