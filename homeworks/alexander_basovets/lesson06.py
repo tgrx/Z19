e528eda3ee7316c18317de692f853f8b4ab51d2a
AlexandrZ1913 <773821@gmail.com>
2019-09-16 17:51:49 +0300

    6 Class

---
+++ homeworks/alexander_basovets/lesson06.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, email={self.email!r})"
+    def compare():
+        if
+
+
+alice = User(name="alice", email="x")
+bob = User(name="bob", email="x")
+
+print(alice)
+print(bob)
+print(alice == bob)
