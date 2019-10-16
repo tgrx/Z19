3109a48adfce116b364a4e1547ffd8b05f2c9ee4
python77777121 <you@example.com>
2019-10-16 13:12:01 +0300

    lesson16 level one added

---
+++ homeworks/oleg_vasilchik/lesson16.py
@@ -0,0 +1,30 @@
+def Zip(text:str):
+
+    output = []
+    letters_counter = dict()
+
+    for i in text:
+        if i in letters_counter:
+            letters_counter[i] += 1
+        else:
+            letters_counter.update({i:1})
+
+    for i in letters_counter:
+        output.append(i)
+        output.append(str(letters_counter[i]))
+
+    return "".join(output)
+
+
+
+def Unzip(text:str):
+
+    output = []
+
+    for i in range(0, len(text)):
+        data = ""
+        if i % 2 == 0:
+            data = text[i]*int(text[i+1])
+            output.append(data)
+    return "".join(output)
+
