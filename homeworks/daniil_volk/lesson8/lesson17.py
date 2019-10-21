10a83eeac48c437683ab6abf3ce70323e0c5f55f
Dalalaler <54038167+Dalalaler@users.noreply.github.com>
2019-10-21 13:48:26 +0300

    Added leeson17 (#137)
    * Added leeson17
    
    * Fixed leeson17

---
+++ homeworks/daniil_volk/lesson8/lesson17.py
@@ -0,0 +1,9 @@
+def compare_triplets(a, b):
+    a_count = 0
+    b_count = 0
+    for i in range(3):
+        if a[i] > b[i]:
+            a_count += 1
+        if a[i] < b[i]:
+            b_count += 1
+    return a_count, b_count
