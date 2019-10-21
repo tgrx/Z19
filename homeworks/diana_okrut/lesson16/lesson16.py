b77af775144b9ce4a0bd08839bbebfe9c502871f
diana-okrut <IHateYOu2014>
2019-10-21 21:38:05 +0300

    l16 Zip/unzip

--- homeworks/diana_okrut/lesson16/lesson16.py
+++ homeworks/diana_okrut/lesson16/lesson16.py
@@ -16,3 +16,18 @@ def Zip(unzip):
             countc += 1
             zip = 'a' + str(counta) + 'b' + str(countb) + 'c' + str(countc)
     return zip
+
+
+def Unzip(zip):
+    if zip == '':
+        return ''
+    unzip = ''
+    if len(zip) == 2:
+        unzip = zip[0] * int(zip[1])
+    elif len(zip) == 4:
+        unzip = zip[0] * int(zip[1]) + zip[2] * int(zip[3])
+    elif len(zip) == 6:
+        unzip = zip[0] * int(zip[1]) + zip[2] * int(zip[3]) + zip[4] * int(zip[5])
+    else:
+        unzip = zip[0] * int(zip[1:3]) + zip[3] * int(zip[4:6]) + zip[6] * int(zip[7])
+    return unzip
