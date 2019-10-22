1a27d192e596d8c3893680dc658ed1967bcccff4
repatik <dalalaler@gmail.com>
2019-10-23 00:04:20 +0300

    Fixed lesson18

--- homeworks/daniil_volk/lesson9/lesson18.py
+++ homeworks/daniil_volk/lesson9/lesson18.py
@@ -1,4 +1,6 @@
 def count_pnz(args):
+    if len(args) == 0:
+        return ("0.000000", "0.000000", "0.000000",)
     count_of_positives = 0
     count_of_negatives = 0
     count_of_nulls = 0
@@ -8,9 +10,15 @@ def count_pnz(args):
             count_of_positives += 1
         if i < 0:
             count_of_negatives += 1
-        else:
-            count_of_nulls += 0
-    total_count.append(str(count_of_positives / len(args)))
-    total_count.append(str(count_of_negatives / len(args)))
-    total_count.append(str(count_of_nulls / len(args)))
-    return tuple(total_count)
+        if i == 0:
+            count_of_nulls += 1
+    count_of_positives = round(count_of_positives / len(args), 6)
+    count_of_negatives =  round(count_of_negatives / len(args), 6)
+    count_of_nulls = round(count_of_nulls / len(args), 6)
+    count_of_positives = str(count_of_positives).ljust(8, '0')
+    count_of_negatives = str(count_of_negatives).ljust(8, '0')
+    count_of_nulls = str(count_of_nulls).ljust(8, '0')
+    print(count_of_positives)
+    print(count_of_negatives)
+    print(count_of_nulls)
+    return (count_of_positives, count_of_negatives, count_of_nulls)
