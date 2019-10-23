0a99886f30df29017af1d051615c8f2da4257901
diana-okrut <IHateYOu2014>
2019-10-23 21:12:45 +0300

    l18 count_pnz

--- homeworks/diana_okrut/lesson18/lesson18.py
+++ homeworks/diana_okrut/lesson18/lesson18.py
@@ -1,16 +1,16 @@
 def count_pnz(m):
     if not m:
         return None
-    d = {'>':0, '<':0, '=': 0}
+    d = {">": 0, "<": 0, "=": 0}
     for x in m:
         if x > 0:
-            d['>'] +=1
+            d[">"] += 1
         elif x < 0:
-            d['<'] += 1
+            d["<"] += 1
         elif x == 0:
-            d['='] += 1
+            d["="] += 1
     res = []
-    res.append(format(d.get('>')/len(m), '.6f'))
-    res.append(format(d.get('<')/len(m), '.6f'))
-    res.append(format(d.get('=')/len(m), '.6f'))
+    res.append(format(d.get(">") / len(m), ".6f"))
+    res.append(format(d.get("<") / len(m), ".6f"))
+    res.append(format(d.get("=") / len(m), ".6f"))
     return tuple(res)
