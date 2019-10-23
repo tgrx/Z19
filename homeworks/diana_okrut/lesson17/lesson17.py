1c8712702d10dcb6a09b2cdc805da9da35e8f921
diana-okrut <IHateYOu2014>
2019-10-23 21:12:36 +0300

    l18 count_pnz

--- homeworks/diana_okrut/lesson17/lesson17.py
+++ homeworks/diana_okrut/lesson17/lesson17.py
@@ -2,8 +2,7 @@ def compare_triplets(A, B):
     res = [0, 0]
     for x, y in zip(A, B):
         if x > y:
-            res[0]+=1
+            res[0] += 1
         elif x < y:
-            res[1]+=1
+            res[1] += 1
     return tuple(res)
-
