06babc237334b455b59dfc3a7a31e8bc7573c73c
repatik <dalalaler@gmail.com>
2019-10-23 20:46:47 +0300

    Blacked

--- homeworks/daniil_volk/lesson9/lesson18.py
+++ homeworks/daniil_volk/lesson9/lesson18.py
@@ -14,7 +14,7 @@ def count_pnz(args):
     count_of_positives = round(count_of_positives / len(args), 6)
     count_of_negatives = round(count_of_negatives / len(args), 6)
     count_of_nulls = round(count_of_nulls / len(args), 6)
-    count_of_positives = str(count_of_positives).ljust(8, '0')
-    count_of_negatives = str(count_of_negatives).ljust(8, '0')
-    count_of_nulls = str(count_of_nulls).ljust(8, '0')
+    count_of_positives = str(count_of_positives).ljust(8, "0")
+    count_of_negatives = str(count_of_negatives).ljust(8, "0")
+    count_of_nulls = str(count_of_nulls).ljust(8, "0")
     return count_of_positives, count_of_negatives, count_of_nulls
