64215868b197f59f9945bd85cd7cfa769e082b8d
diana-okrut <IHateYOu2014>
2019-10-23 20:05:58 +0300

    l17 compare_triplets

--- homeworks/diana_okrut/lesson16/lesson16.py
+++ homeworks/diana_okrut/lesson16/lesson16.py
@@ -1,21 +1,21 @@
-import string
-
-def Zip(unzip):
-    if not unzip:
-        return ""
-    zip = ""
-    for i in unzip:
-        if i in string.ascii_letters:
-
-        elif i in string.digits:
-
-    return zip
-
-
-def Unzip(zip):
-    if  not zip:
-        return ""
-    unzip = ""
-    for i in zip:
-
-    return unzip
+# import string
+#
+# def Zip(unzip):
+#     if not unzip:
+#         return ""
+#     zip = ""
+#     for i in unzip:
+#         if i in string.ascii_letters:
+#
+#         elif i in string.digits:
+#
+#     return zip
+#
+#
+# def Unzip(zip):
+#     if  not zip:
+#         return ""
+#     unzip = ""
+#     for i in zip:
+#
+#     return unzip
