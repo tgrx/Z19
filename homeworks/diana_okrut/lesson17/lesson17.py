0f30fbbd7ae2f29efb2e86b127cb683a4ebada1f
diana-okrut <47084900+diana-okrut@users.noreply.github.com>
2019-10-23 21:35:23 +0300

    compare_triplets /count_pnz (#142)
    * l18 count_pnz
    
    * compare_triplets
    
    * count_pnz
    
    * count_pnz
    
    * count_pnz
    
    * count_pnz
    
    * Delete __init__.py

---
+++ homeworks/diana_okrut/lesson17/lesson17.py
@@ -0,0 +1,8 @@
+def compare_triplets(A, B):
+    res = [0, 0]
+    for x, y in zip(A, B):
+        if x > y:
+            res[0] += 1
+        elif x < y:
+            res[1] += 1
+    return tuple(res)
