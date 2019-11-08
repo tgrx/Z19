3de9e3dda09490cc5beb0326f56ecaf85c1600e9
Maximilian Sklyar <maximilian.sklyar@gmail.com>
2019-11-08 20:53:14 +0300

    Lesson 18 commit

---
+++ homeworks/maximilian_sklyar/lesson18.py
@@ -0,0 +1,11 @@
+def count_pnz(a):
+    p = 1 / len(a)
+    r = [0, 0, 0]
+    for e in a:
+        if e > 0:
+            r[0] += p
+        if e > 0:
+            r[1] += p
+        if e == 0:
+            r[2] += p
+    return tuple("{0:.6f}".format(e) for e in r)
