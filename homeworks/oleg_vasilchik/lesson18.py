ab426f3172d346dba02c9b235ca44308041cf89d
python77777121 <you@example.com>
2019-11-09 13:18:00 +0300

    lesson 18 added

---
+++ homeworks/oleg_vasilchik/lesson18.py
@@ -0,0 +1,21 @@
+def count_pnz(massive):
+    output = []
+    total = len(massive)
+    minus, plus, zero = 0, 0, 0
+
+    for i in massive:
+        if i > 0:
+            plus += 1
+        elif i < 0:
+            minus += 1
+        else:
+            zero += 1
+
+    share_plus = '{:.6f}'.format(plus / total)
+    share_minus = '{:.6f}'.format(minus / total)
+    share_zero = '{:.6f}'.format(zero / total)
+
+    output.append(str(share_plus))
+    output.append(str(share_minus))
+    output.append(str(share_zero))
+    print(output)
