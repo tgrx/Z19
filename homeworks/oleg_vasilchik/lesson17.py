6632d5ee86dcc4e528fe992deca0469242ff12c4
Oleg Vasilchik <54038183+python77777121@users.noreply.github.com>
2019-11-09 15:27:43 +0300

    L 17 (#148)
    * lesson16 level one added
    
    * lesson 16 added
    
    * lesson 16 added
    
    * lesson 16 added
    
    * lesson 16
    
    * 16 lesson
    
    * try to fix
    
    * fixxx
    
    * fix
    
    * zip unzip
    
    * zip unzip22
    
    * zip unzip3
    
    * zip unzip4
    
    * zip unzip5
    
    * and again
    
    * lesson17 try
    
    * lesson17 fixed
    
    * try again

---
+++ homeworks/oleg_vasilchik/lesson17.py
@@ -0,0 +1,17 @@
+def compare_triplets(A, B):
+    output = []
+    score_a = 0
+    score_b = 0
+
+    for a, b in zip(A, B):
+        if a > b:
+            score_a += 1
+        elif a < b:
+            score_b += 1
+        else:
+            continue
+
+    output.append(score_a)
+    output.append(score_b)
+
+    return tuple(output)
