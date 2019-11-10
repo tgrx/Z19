48c756c35ae2bad2498a543059d7875e31d52f24
AlexandrZ1913 <773821@gmail.com>
2019-11-10 21:36:54 +0300

    lesson17

---
+++ homeworks/alexander_basovets/lesson17.py
@@ -0,0 +1,11 @@
+def compare_triplets(A, B):
+    i = 0
+    c = [0, 0]
+    while i <= 2:
+        if A[i] > B[i]:
+            c[0] = c[0] + 1
+        elif A[i] < B[i]:
+            c[1] = c[1] + 1
+        i += 1
+    m = tuple(c)
+    return m
