b483dd5f5b088ab3449629412f76e3f53a87388f
AlexandrZ1913 <773821@gmail.com>
2019-11-10 22:52:02 +0300

    lesson18

---
+++ homeworks/alexander_basovets/lesson18.py
@@ -0,0 +1,24 @@
+def count_pnz(x):
+    y = len(x) - 1
+    s = len(x)
+    i = 0
+    a = 0
+    b = 0
+    c = 0
+    d = [""]
+    while i <= y:
+        if x[i] > 0:
+            a += 1
+        elif x[i] < 0:
+            b += 1
+        else:
+            c += 1
+        i += 1
+    af = a / s
+    bf = b / s
+    cf = c / s
+    d[0] = format(af, ".6f")
+    d.append(format(bf, ".6f"))
+    d.append(format(cf, ".6f"))
+    m = tuple(d)
+    return m
