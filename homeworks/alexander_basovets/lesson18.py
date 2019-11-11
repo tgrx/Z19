c38b87ab78f1f1cb59c9e0db66c514847541b49d
AlexandrZ1913 <773821@gmail.com>
2019-11-11 17:46:00 +0300

    lesson 18-zero

--- homeworks/alexander_basovets/lesson18.py
+++ homeworks/alexander_basovets/lesson18.py
@@ -6,19 +6,22 @@ def count_pnz(x):
     b = 0
     c = 0
     d = [""]
-    while i <= y:
-        if x[i] > 0:
-            a += 1
-        elif x[i] < 0:
-            b += 1
-        else:
-            c += 1
-        i += 1
-    af = a / s
-    bf = b / s
-    cf = c / s
-    d[0] = format(af, ".6f")
-    d.append(format(bf, ".6f"))
-    d.append(format(cf, ".6f"))
-    m = tuple(d)
+    if x != []:
+        while i <= y:
+            if x[i] > 0:
+                a += 1
+            elif x[i] < 0:
+                b += 1
+            else:
+                c += 1
+            i += 1
+        af = a / s
+        bf = b / s
+        cf = c / s
+        d[0] = format(af, ".6f")
+        d.append(format(bf, ".6f"))
+        d.append(format(cf, ".6f"))
+        m = tuple(d)
+    else:
+        m = tuple(d)
     return m
