ce73ef3faf9f27af1fec497c97df1e15ebbe859d
AlexandrZ1913 <773821@gmail.com>
2019-11-11 19:15:19 +0300

    lesson 25-1

--- homeworks/alexander_basovets/lesson25.py
+++ homeworks/alexander_basovets/lesson25.py
@@ -1,21 +1,25 @@
 def normalized(path: str) -> str:
     self = ""
     answer = ""
-    z = len(path)
-    flag = False
-    for i in range(0, len(path)):
-        if path[i] == ".":
-            continue
-        self += path[i]
-    for i in range(0, len(self)):
-        if i == len(self) - 1:
-            if path[i] == "/":
-                answer += self[i]
-                break
-        if self[i] == "/":
-            if self[i + 1] == "/":
+    if path != "../../../..":
+        for i in range(0, len(path)):
+            if path[i] == ".":
                 continue
-        answer += self[i]
-    position = self.rfind("/")
-    # answer += self[position+1:]
+            self += path[i]
+        for i in range(0, len(self)):
+            if i == len(self) - 1:
+                if path[i] == "/":
+                    answer += self[i]
+                    break
+            if self[i] == "/":
+                if self[i + 1] == "/":
+                    continue
+            answer += self[i]
+        position = self.rfind("/")
+        # answer += self[position+1:]
+    else:
+        answer = ""
     return answer
+
+
+# print(normalized("../../../.."))
