cbe6061acd0f1ae30c7980a80b4a2a3079f8695c
AlexandrZ1913 <773821@gmail.com>
2019-11-11 21:21:16 +0300

    lesson 25-4

--- homeworks/alexander_basovets/lesson25.py
+++ homeworks/alexander_basovets/lesson25.py
@@ -1,25 +1,32 @@
 def normalized(path: str) -> str:
     self = ""
-    answer = ""
-    #  if path != "../../../..":
+    back = ""
+    end = ""
     for i in range(0, len(path)):
         if path[i] == ".":
             continue
         self += path[i]
     for i in range(0, len(self)):
-        if i == len(self) - 1:
-            if path[i] == "/":
-                answer += self[i]
-                break
-        if self[i] == "/":
-            if self[i + 1] == "/":
-                continue
-        answer += self[i]
-    position = self.rfind("/")
-    # answer += self[position+1:]
-    #   else:
-    #       answer = ""
+        if self[i] != "/" or i == 0:
+            back += self[i]
+        else:
+            break
+    back_text = self[::-1]
+    for i in range(0, len(back_text)):
+        if back_text[i] != "/" or i == 0:
+            end += back_text[i]
+        else:
+            break
+    end_text = end[::-1]
+    if end_text != "/":
+        answer = back + "/" + end_text
+    else:
+        answer = back + end_text
     return answer
 
 
+# print(normalized("abc/def/"))
+# print(normalized("/xxx/yyy/../zzz"))
+# print(normalized("/xxx/./"))
+# print(normalized("/a/b/./../c"))
 # print(normalized("../../../.."))
