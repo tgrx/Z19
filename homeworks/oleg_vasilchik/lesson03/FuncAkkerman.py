6f4721693950c1c5548e38da30a6ac6d5918ae2c
Oleg Vasilchik <54038183+python77777121@users.noreply.github.com>
2019-11-12 14:18:49 +0300

    l3homework (#156)

---
+++ homeworks/oleg_vasilchik/lesson03/FuncAkkerman.py
@@ -0,0 +1,11 @@
+n = int(input("Введите первое число: "))
+m = int(input("Введите второе число: "))
+
+
+def func(n, m):
+    if m == 0:
+        return n + 1
+    if m > 0 and n == 0:
+        return func(m - 1, 1)
+    if m > 0 and n > 0:
+        return func(m - 1, func(m, n - 1))
