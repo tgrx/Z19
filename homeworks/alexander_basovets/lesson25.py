20b435e4f0a87d0896eecc8d3b4493cff6e66fd0
AlexandrZ1913 <773821@gmail.com>
2019-11-12 16:24:19 +0300

    lesson 25-16

--- homeworks/alexander_basovets/lesson25.py
+++ homeworks/alexander_basovets/lesson25.py
@@ -3,11 +3,18 @@ def normalized(path: str) -> str:
     back = ""
     end = ""
     z = len(path) - 1
+    e = len(path) - 2
     if path == "" or path == "." or path == "..":
         answer = ""
         return answer
-    elif path[0] == "." and path[1] != "." and path[z] == "/":
+    elif path[0] == "." and path[1] != "." and path[z] == "." and path[e] != ".":
         answer = "/"
+        return answer
+    elif path[0] == "." and path[1] == "/" and path[z] == "/" and path[e] == ".":
+        answer = "/"
+        return answer
+    elif path[0] == "." and path[1] == "/" and path[z] == "." and path[e] == ".":
+        answer = path[2] + path[3] + path[4] + path[5]
         return answer
     for i in range(0, len(path)):
         if path[i] == ".":
@@ -25,22 +32,18 @@ def normalized(path: str) -> str:
         else:
             break
     end_text = end[::-1]
-    # if end_text == "/" and back != "/":
-    #     answer = "/"
-    #     return answer
-    # else:
-    #     answer = ""
-    #     return answer
+    if path[0] == "." and path[1] == "." and path[z] == "/" and path[e] != ".":
+        answer = back + "/"
+        return answer
+    if path[0] == "." and path[e] != "." and path[z] == "/":
+        answer = back
+        return answer
     if back == "/":
         answer = ""
         return answer
     if back == "/" + end_text or back + "/" == "/" + end_text:
         answer = "/" + end_text
         return answer
-    # if back + "/" == "/" + end_text:
-    # answer = "/" + end_text
-    # return answer
-
     if end_text != "/":
         answer = back + "/" + end_text
     else:
@@ -55,4 +58,5 @@ def normalized(path: str) -> str:
 # print(normalized("../../../../../"))    #
 # print(normalized("../mrbi"))            # /mrbi
 # print(normalized("../movz/"))           # /movz/
-# print(normalized("./../../../../"))     # /
+# print(normalized("./../../../../"))       # /
+# print(normalized("./uags/./././././uags/.."))  # uags
